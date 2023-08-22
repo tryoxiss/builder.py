add9dee6c3107b835bdca344ec3279569fddc2df
tryoxiss <tryoxiss@gmail.com>
2023-08-22 16:50:55 -0700

    🚧 AAAA

--- builder_modules/api.py
+++ builder_modules/api.py
@@ -1,1 +1,9 @@
 # The API you have access to by default in <?py/> blocks.
+
+# Should probably be core not api? idk
+# def paginate(per_page: int, posts: list):
+#     """
+    
+#     """
+
+#     pass
