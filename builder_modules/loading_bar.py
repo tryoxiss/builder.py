4d839a154b654c099f635e19905ed76f3ad4aea4
tryoxiss <tryoxiss@gmail.com>
2023-08-21 20:15:09 -0700

    🚧 Colors for the loading bar

--- builder_modules/loading_bar.py
+++ builder_modules/loading_bar.py
@@ -6,6 +6,11 @@ class LoadingBar:
 
     _up_line = "\033[F"
 
+    loaded_color = "\033[92m"
+    unloaded_color = "\033[30m"
+    symbol_color = "\033[37m"
+    text_color = "\033[97m"
+
     def __init__(self, required_progress: int, first_task):
         self.progress = 0
         self.total = required_progress
@@ -25,13 +30,13 @@ class LoadingBar:
         bar_competed_characters = round(completed_percent / 2, 0)
 
         if completed_percent == 0:
-            print("")
+            print("\033[0m")
 
-        print(f"{self._up_line}[{'#' * int(bar_competed_characters)}{(50 - int(bar_competed_characters)) * '.'}] {round(completed_percent, 1)}%")
-        print(f"{task}{' ' * self._previous_length}{self._up_line}")
+        print(f"{self._up_line}{self.symbol_color}[{self.loaded_color}{'#' * int(bar_competed_characters)}{self.unloaded_color}{(50 - int(bar_competed_characters)) * '.'}{self.symbol_color}] {self.text_color}{round(completed_percent, 1)}{self.symbol_color}%")
+        print(f"{self.text_color}{task}{' ' * self._previous_length}{self._up_line}")
 
         # >= cause python jank
         if completed_percent >= 100:
-            print("") # Add a new line
+            print("\033[0m") # Add a new line
         
         self._previous_length = len(task)
