1516c74e014eb7e71ef32790067a6e2177aa4d72
tryoxiss <tryoxiss@gmail.com>
2023-10-12 21:17:24 -0700

    🚧 Stress tests

--- runme.py
+++ runme.py
@@ -6,9 +6,13 @@ import modules.config as config
 class BuilderPyExample(builder.Builder):
 	pass
 
+import time
+
 # You can specify your compilers with keyword arguments, or override thier functions!
 # We have provided a few default values though. If they are too heavy for your liking, feel free
 # to edit `modules/builder.py` and remove them!
 # BuilderPyExample().blueprint()
 
+# start = time.time_ns()
 builder.Builder().build_all(config.content.directory, 0)
+# print(f"This took {(time.time_ns() - start) / 1000}mcs or {(time.time_ns() - start) / 1_000_000}ms")
